96191e3dbf0d97bd72f5b5dfd673ed9013df88ca
anpenalo <anpenalo@bancolombia.com.co>
2024-04-11 23:26:21 -0500

    Generic repositories and unitofwork, rawmaterialcontroller

---
+++ Restromanager/Restromanager.Backend/Controllers/GenericController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restromanager.Backend.UnitsOfWork.interfaces;
+
+namespace Restromanager.Backend.Controllers
+{
+    public class GenericController<T>(IGenericUnitOfWork<T> unitOfWork) : Controller where T : class
+    {
+        private readonly IGenericUnitOfWork<T> _unitOfWork = unitOfWork;
+
+        [HttpGet]
+        public virtual async Task<IActionResult> GetAsync()
+        {
+            var action = await _unitOfWork.GetAsync();
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return BadRequest();
+        }
+        [HttpGet("{id}")]
+        public virtual async Task<IActionResult> GetAsync(int id)
+        {
+            var action = await _unitOfWork.GetAsync(id);
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return BadRequest();
+        }
+        [HttpPost]
+        public virtual async Task<IActionResult> PostAsync(T model)
+        {
+            var action = await _unitOfWork.AddAsync(model);
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return BadRequest();
+        }
+        [HttpPut]
+        public virtual async Task<IActionResult> PutAsync(T model)
+        {
+            var action = await _unitOfWork.UpdateAsync(model);
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return BadRequest();
+        }
+        [HttpDelete("{id}")]
+        public virtual async Task<IActionResult> DeleteAsync(int id)
+        {
+            var action = await _unitOfWork.DeleteAsync(id);
+            if (action.WasSuccess)
+            {
+                return NoContent();
+            }
+            return NotFound();
+        }
+
+    }
+}
