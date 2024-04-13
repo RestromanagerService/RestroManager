9276a05b28dc556455d2d02ef0e5ab5b96f91001
anpenalo <anpenalo@bancolombia.com.co>
2024-04-12 22:31:13 -0500

    Cities controller

---
+++ Restromanager/Restromanager.Backend/Controllers/CitiesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restromanager.Backend.Domain.Entities;
+using Restromanager.Backend.UnitsOfWork.interfaces;
+
+namespace Restromanager.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CitiesController(IGenericUnitOfWork<City> unitOfWork) : GenericController<City>(unitOfWork)
+    {
+    }
+}
