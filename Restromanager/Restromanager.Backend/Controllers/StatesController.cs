fbc300edba0663fe3dbabe5a835afb717bbe9e9c
anpenalo <anpenalo@bancolombia.com.co>
2024-04-12 22:32:51 -0500

    States controller

---
+++ Restromanager/Restromanager.Backend/Controllers/StatesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restromanager.Backend.Domain.Entities;
+using Restromanager.Backend.UnitsOfWork.interfaces;
+
+namespace Restromanager.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatesController(IGenericUnitOfWork<State> unitOfWork) : GenericController<State>(unitOfWork)
+    {
+    }
+}
