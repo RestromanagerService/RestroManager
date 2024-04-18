eba916909b93879ac9a20cb29b94fab91df901fd
anpenalo <anpenalo@bancolombia.com.co>
2024-04-17 19:14:07 -0500

    cambios a program

--- Restromanager/Restromanager.Backend/Program.cs
+++ Restromanager/Restromanager.Backend/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=OskarConnection"));
 builder.Services.AddTransient<SeedDb>();
 builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
