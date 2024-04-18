a6b8b8e2db336effb2bdb4dff6e4dfe897b73774
anpenalo <anpenalo@bancolombia.com.co>
2024-04-17 19:27:42 -0500

    aprendiendo git

--- Restromanager/Restromanager.Backend/Program.cs
+++ Restromanager/Restromanager.Backend/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=OskarConnection"));
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=Oskar2AprendiendoGItConnection"));
 builder.Services.AddTransient<SeedDb>();
 builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
