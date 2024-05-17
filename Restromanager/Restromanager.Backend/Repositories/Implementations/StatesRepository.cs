0a63f064bed57fc8cee8f8272433ef1cde173980
sebbppe <sebbppe@gmail.com>
2024-05-16 22:02:01 -0500

    delete passwords

--- Restromanager/Restromanager.Backend/Repositories/Implementations/StatesRepository.cs
+++ Restromanager/Restromanager.Backend/Repositories/Implementations/StatesRepository.cs
@@ -10,13 +10,14 @@ namespace Restromanager.Backend.Repositories.Implementations
 {
     public class StatesRepository(DataContext dataContext) : GenericRepository<State>(dataContext), IStatesRepository
     {
-        private readonly DataContext _context=dataContext;
+        private readonly DataContext _context = dataContext;
         public override async Task<ActionResponse<State>> GetAsync(int id)
         {
             var state = await _context.States
-                .Include(s=>s.Cities!)
-                .FirstOrDefaultAsync(s=>s.Id==id);
-            if (state == null) {
+                .Include(s => s.Cities!)
+                .FirstOrDefaultAsync(s => s.Id == id);
+            if (state == null)
+            {
                 return new ActionResponse<State>
                 {
                     WasSuccess = false,
@@ -26,16 +27,16 @@ namespace Restromanager.Backend.Repositories.Implementations
             return new ActionResponse<State>
             {
                 WasSuccess = true,
-                Result =state
+                Result = state
             };
 
         }
 
         public override async Task<ActionResponse<IEnumerable<State>>> GetAsync()
         {
-            var states= await _context.States
-                .OrderBy(s=>s.Name)
-                .Include(s=>s.Cities)
+            var states = await _context.States
+                .OrderBy(s => s.Name)
+                .Include(s => s.Cities)
                 .ToListAsync();
             return new ActionResponse<IEnumerable<State>>
             {
@@ -47,7 +48,7 @@ namespace Restromanager.Backend.Repositories.Implementations
         {
             var queryable = _context.States
                 .Include(s => s.Cities)
-                .Where(s=>s.Country!.Id==pagination.Id)
+                .Where(s => s.Country!.Id == pagination.Id)
                 .AsQueryable();
             return new ActionResponse<IEnumerable<State>>
             {
@@ -61,7 +62,7 @@ namespace Restromanager.Backend.Repositories.Implementations
         public override async Task<ActionResponse<int>> GetTotalPagesAsync(PaginationDTO pagination)
         {
             var queryable = _context.States
-                .Where(s=>s.CountryId==pagination.Id)
+                .Where(s => s.CountryId == pagination.Id)
                 .AsQueryable();
             double count = await queryable.CountAsync();
             int totalPages = (int)Math.Ceiling(count / pagination.RecordsNumber);
@@ -69,6 +70,26 @@ namespace Restromanager.Backend.Repositories.Implementations
             {
                 WasSuccess = true,
                 Result = totalPages
+            };
+        }
+        public async Task<ActionResponse<IEnumerable<State>>> GetComboAsync(int countryId)
+        {
+            var states = await _context.States
+                .Where(s => s.CountryId == countryId)
+                .OrderBy(s => s.Name).ToListAsync();
+            return new ActionResponse<IEnumerable<State>>
+            {
+                WasSuccess = true,
+                Result = states
+            };
+        }
+        public async Task<ActionResponse<IEnumerable<Country>>> GetComboAsync()
+        {
+            var countries = await _context.Countries.OrderBy(c => c.Name).ToListAsync();
+            return new ActionResponse<IEnumerable<Country>>
+            {
+                WasSuccess = true,
+                Result = countries
             };
         }
 
